d24282bf88b8c97010f4ed8c2cc8e0f76c3604ed
Christopher Moore <christopher.e.moore@gmail.com>
2017-07-17 20:10:04 +0300

    got things to build with luamake

--- Include.mk
+++ Include.mk
@@ -3,10 +3,11 @@ GLAPP_PATH:=$(dir $(lastword $(MAKEFILE_LIST)))
 
 INCLUDE+=$(GLAPP_PATH)/include
 DYNAMIC_LIBS+=$(GLAPP_PATH)dist/$(PLATFORM)/$(BUILD)/libGLApp$(LIB_SUFFIX)
-#DYNAMIC_LIBS+=$(HOME)/lib/libSDL2-2.0.0$(LIB_SUFFIX)
-DYNAMIC_LIBS_linux+=/usr/lib/x86_64-linux-gnu/libSDL2-2.0$(LIB_SUFFIX)
+DYNAMIC_LIBS+=$(HOME)/lib/libSDL2-2.0.0$(LIB_SUFFIX)
+INCLUDE_osx+=$(HOME)/include/SDL2
+#DYNAMIC_LIBS_linux+=/usr/lib/x86_64-linux-gnu/libSDL2-2.0$(LIB_SUFFIX)
 LDFLAGS_osx+= -framework Cocoa -framework OpenGL
 #LIBS_linux+=GL GLU
-DYNAMIC_LIBS_linux+=/usr/lib/x86_64-linux-gnu/libGL$(LIB_SUFFIX)
-DYNAMIC_LIBS_linux+=/usr/lib/x86_64-linux-gnu/libGLU$(LIB_SUFFIX)
+#DYNAMIC_LIBS_linux+=/usr/lib/x86_64-linux-gnu/libGL$(LIB_SUFFIX)
+#DYNAMIC_LIBS_linux+=/usr/lib/x86_64-linux-gnu/libGLU$(LIB_SUFFIX)
 #LIBS+=SDL2main
--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 DIST_FILENAME=GLApp
 DIST_TYPE=lib
 include ../Common/Base.mk
+INCLUDE_osx+=$(HOME)/include/SDL2
