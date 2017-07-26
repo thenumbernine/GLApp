4182eb8933280f841430a19553fef5db7a08025e
Christopher Moore <christopher.e.moore@gmail.com>
2017-07-26 19:53:19 +0300

    more platform stuff

--- Include.mk
+++ Include.mk
@@ -3,8 +3,10 @@ GLAPP_PATH:=$(dir $(lastword $(MAKEFILE_LIST)))
 
 INCLUDE+=$(GLAPP_PATH)/include
 DYNAMIC_LIBS+=$(GLAPP_PATH)dist/$(PLATFORM)/$(BUILD)/libGLApp$(LIB_SUFFIX)
-DYNAMIC_LIBS+=$(HOME)/lib/libSDL2-2.0.0$(LIB_SUFFIX)
+DYNAMIC_LIBS_osx+=$(HOME)/lib/libSDL2-2.0.0$(LIB_SUFFIX)
+LIBS_linux+=GL SDL2
 INCLUDE_osx+=$(HOME)/include/SDL2
+INCLUDE_linux+=/usr/include/SDL2
 #DYNAMIC_LIBS_linux+=/usr/lib/x86_64-linux-gnu/libSDL2-2.0$(LIB_SUFFIX)
 LDFLAGS_osx+= -framework Cocoa -framework OpenGL
 #LIBS_linux+=GL GLU
